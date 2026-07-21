634e26afef8eaa9517e374fe37469e3701357f71
Vijay Solanki <vijaysolankia0@gmail.com>
2026-07-21 12:55:46 +0530

    fix username and email in user login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -100,7 +100,7 @@ const loginUser = asyncHandler( async(req,res) => {
 
     const {username, email, password} = req.body;
 
-    if(!username || !email){
+    if(!(username || email)){
         throw new ApiError(400, "username or email is required")
     }
 
