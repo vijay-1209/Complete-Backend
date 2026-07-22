44914db2e6532427d958f9a94986e3cf1a9d9c08
Vijay Solanki <vijaysolankia0@gmail.com>
2026-07-22 10:44:37 +0530

    add updateUserCoverImage function to handle cover image uploads

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -279,6 +279,32 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
     .json(200, user, "user avatar updated successfully")
 })
 
+const updateUserCoverImage = asyncHandler(async(req,res) => {
+    const coverImageLocalPath = req.file?.path
+    if(!coverImageLocalPath){
+        throw new ApiError(400,"cover file is missing")
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    if(!coverImage.url){
+        throw new ApiError(400,"Error while uploading cover image")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage: coverImage.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(200, user, "user cover image updated successfully")
+})
+
 export { 
     registerUser,
     loginUser,
